ac3f6d6bacd8279c9d6a6b2c9c0878bd3e5bf325
hopeorno <joodaljohani11@hotmail.com>
2026-04-05 17:24:09 +0300

    bind to 0.0.0.0

--- server.js
+++ server.js
@@ -199,4 +199,5 @@ app.get('/profile', checkLogin, async (req, res) => {
 
 // ===== SERVER =====
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running'));
+app.listen(PORT, "0.0.0.0", () => console.log("Server running"));
+
